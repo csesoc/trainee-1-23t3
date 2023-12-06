79d4ae5a2d5cebb36610394c1305cf6f686cfc16
Jayden Nguyen <jayden.m.nguyen@student.unsw.edu.au>
2023-12-06 22:20:47 +1100

    local change

--- src/pages/Catalogue.tsx
+++ src/pages/Catalogue.tsx
@@ -1,7 +1,11 @@
+import { Route } from "react-router-dom";
+
 export default function Catalogue() {
     return (
-        <div className="flex flex-col items-center justify-center">
-          <h1>Slay</h1>
-        </div>
+      <div className="flex flex-col items-center justify-center">
+        <button>
+          slay
+        </button>
+      </div>
     );
 }
