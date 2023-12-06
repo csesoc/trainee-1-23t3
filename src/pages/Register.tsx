02cb8afdfc8a54da9e0086c4b3ea4108b2001fbc
Jayden Nguyen <jayden.m.nguyen@student.unsw.edu.au>
2023-12-07 00:11:39 +1100

    fixed a bit of frontend

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -22,15 +22,15 @@ export default function Register() {
   return (
     <div className="flex items-center justify-center">
       <div className="flex w-200 w-[90%] h-screen justify-center items-center">
-        <div className="bg-white w-[70%] h-[86%] text-black rounded-[30px]">
-        <div className={`${errorMessage ? 'bg-red-500 text-white text-center p-2 rounded mx-auto my-2 max-w-sm' : 'invisible'} my-2 h-10`}>
-            {errorMessage}
-        </div>
-          <h1 className="mb-3 text-4xl font-bold text-left" style={{ paddingLeft: '220px', paddingTop: '30px', paddingBottom: '32px' }}>Sign up</h1>
-          <div className="flex flex-col items-center justify-center">
-            <input className="w-[50%] text-2xl mb-5 pb-3 focus:outline-none bg-transparent border-b-2 border-black" placeholder="Username" onChange={e => setUsername(e.target.value)} />
-            <input className="w-[50%] text-2xl mb-5 pb-3 focus:outline-none bg-transparent border-b-2 border-black" placeholder="Email" onChange={e => setEmail(e.target.value)} />
-            <div className="relative w-[50%]">
+        <div className="flex flex-col justify-center bg-white shadow-2xl w-[70%] h-[86%] text-black rounded-[30px]">
+          <div className={`${errorMessage ? 'bg-red-500 text-white text-center p-2 rounded mx-auto my-2 max-w-sm' : 'invisible'} my-2 h-10`}>
+              {errorMessage}
+          </div>
+          <div className="flex flex-col items-center justify-center mb-[5%]">
+            <h1 className="w-[55%] mb-[3%] text-4xl font-bold text-left" style={{}}>Sign up</h1>
+            <input className="w-[55%] text-2xl mb-5 pb-3 focus:outline-none bg-transparent border-b-2 border-black" placeholder="Username" onChange={e => setUsername(e.target.value)} />
+            <input className="w-[55%] text-2xl mb-5 pb-3 focus:outline-none bg-transparent border-b-2 border-black" placeholder="Email" onChange={e => setEmail(e.target.value)} />
+            <div className="relative w-[55%]">
               <input id="password-input" type={showPassword ? "text" : "password"} className="w-[100%] text-2xl mb-5 pb-3 focus:outline-none bg-transparent border-b-2 border-black" placeholder="Password" onChange={e => setPassword(e.target.value)} />
               <button id="toggle-button" type="button" data-toggle-password='{ "target": "#password-input" }' className="absolute -top-2 -end-2 p-3.5 rounded-e-md focus:outline-none" onClick={() => setShowPassword(!showPassword)}>
                 <svg className="flex-shrink-0 w-5 h-5 text-gray-400 dark:text-neutral-600" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
@@ -43,8 +43,8 @@ export default function Register() {
                 </svg>
               </button>
             </div>
-            <div className="relative w-[50%] mb-10">
-              <input id="confirmPassword-input" type={showPassword ? "text" : "password"} className="w-[100%] text-2xl mb-5 pb-3 focus:outline-none bg-transparent border-b-2 border-black" placeholder="Confirm Password" onChange={e => setConfirmPassword(e.target.value)} />
+            <div className="relative w-[55%] mb-[10]">
+              <input id="confirmPassword-input" type={showPassword ? "text" : "password"} className="w-[100%] text-2xl mb-[10%] pb-3 focus:outline-none bg-transparent border-b-2 border-black" placeholder="Confirm Password" onChange={e => setConfirmPassword(e.target.value)} />
               <button id="toggle-button" type="button" data-toggle-password='{ "target": "#password-input" }' className="absolute -top-2 -end-2 p-3.5 rounded-e-md focus:outline-none" onClick={() => setShowPassword(!showPassword)}>
                 <svg className="flex-shrink-0 w-5 h-5 text-gray-400 dark:text-neutral-600" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                   <path d="M9.88 9.88a3 3 0 1 0 4.24 4.24" />
@@ -57,7 +57,7 @@ export default function Register() {
               </button>
             </div>
             <button className="w-[40%] bg-[#61296f] hover:bg-[#311538] text-xl text-white font-bold py-4 px-4 rounded-full" onClick={handleSubmit}>Submit</button>
-            <div className="flex mt-14 gap-2">
+            <div className="flex mt-[3%] gap-2">
               <p>Already signed up?</p>
               <a className="font-bold" href="/login">Log in</a>
             </div>
