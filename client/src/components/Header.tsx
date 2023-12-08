eee10f81df523c8bcdf41d5f4c705881bf3fb493
jroxasqq <yourName@student.unsw.edu.au>
2023-12-08 22:11:07 +1100

    Added login and register link to header component

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,7 +1,19 @@
 const Header = () => {
 	return (
-		<div className="flex bg-[#5a72bc] w-[100%] h-[10%] text-white rounded-t-[30px] text-left items-center">
+		<div className="flex bg-[#5a72bc] w-[100%] h-[10%] text-white rounded-t-[30px] text-left items-center justify-between">
 			<p className="ml-5 text-3xl font-extrabold">StuHDySpaces</p>
+			<div className="flex mr-5 gap-5">
+				<a
+					className="text-xl font-extrabold"
+					href="/login">
+					Login
+				</a>
+				<a
+					className="text-xl font-extrabold"
+					href="/login">
+					Register
+				</a>
+			</div>
 		</div>
 	)
 }
